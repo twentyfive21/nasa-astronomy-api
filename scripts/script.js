7475d49ba04ea017ead82b046cc1f9be5901df0a
Tina <tina.winnn@gmail.com>
2023-06-13 15:57:24 -0400

    changing implementating of code

--- scripts/script.js
+++ scripts/script.js
@@ -1,64 +1,24 @@
-import key from "./key.js";
-const dailyImage = document.getElementById("daily-btn");
-const searchImage = document.getElementById("search-btn");
-const searchDate = document.getElementById("search-date");
-const image = document.getElementById("image");
-const imageDate = document.getElementById("image-date");
-const imageTitle = document.getElementById("image-title");
-const imageDescription = document.getElementById("image-description");
-const detailsContainer = document.querySelector(".details-container");
-// console.log(detailsContainer)
+function getInput () {
+ const searchBtn = document.getElementById("search-btn"); // a link 
+ const input = document.getElementById("search-date");
+ const invalid = document.getElementById("invalid");
+ const oldestDate = new Date("1995-06-20");
+ const currentDate = new Date();
+ 
+ input.addEventListener("input", (event) => {
+    const selectedDate = new Date(event.target.value);
 
-function getAstronomy () {
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
-    .then((res) => res.json())
-    .then((json) => {
-        const astronomyData = json;
-        displayAstronomy(astronomyData);
-        console.log(astronomyData);
-    })
-    .catch((error) => {{
-        console.log("error api down");
-        imageError();
-    }});
-}
-// getAstronomy();
-// displays the images and data fetched 
-function displayAstronomy (astronomyData) {
-    setImg(image, `${astronomyData.url}`);
-    setText(imageDate, `${astronomyData.date}`);
-    setText(imageTitle, `${astronomyData.title}`);
-    setText(imageDescription, `${astronomyData.explanation}`);
-    console.log(astronomyData.url);
-    console.log(astronomyData.date);
-    console.log(astronomyData.title);
-    console.log(astronomyData.explanation);
-}
-
-
-// sets image
-function setImg (element, imgSrc) {
-    if(imgSrc === undefined){
-        element.setAttribute("src", imgSrc);
+    if (selectedDate > currentDate) {
+        invalid.innerHTML = "Please select a date not greater than the current day."
+    } else if (selectedDate < oldestDate) {
+        invalid.innerHTML = "NASA does not have any images before this date";
+    } else {
+    searchBtn.href = `details.html?&date=${event.target.value}`; // date
     } 
-     element.setAttribute("src", imgSrc);
-    }
+ })
+}
 
-// sets text
-function setText (element, string ) {
-    element.innerText = string;
-     }
+getInput();
 
-// displays when nasa api is down 
-function imageError () {
-    detailsContainer.innerHTML = `<img src="/images/filler.jpeg" alt="astronomy image" id="image">
-    <section>
-        <h1 id="image-date">2020 January 5</h1>
-        <h3 id="image-title">A Starry Night of Iceland</h3>
-        <p class="error"> NASA's data is down temporarily. Here is the picture taken on January meanwhile the site is being maintainced. Please check back later! </p><br>
-        <p id="image-description">
-            Like an illustration in a galactic Just So Story, the Elephant's Trunk Nebula winds through the emission region and young star cluster complex IC 1396, in the high and far off constellation of Cepheus. Seen on the left the cosmic elephant's trunk, also known as vdB 142, is over 20 light-years long. This detailed telescopic view features the bright swept-back ridges and pockets of cool interstellar dust and gas that abound in the region. But the dark, tendril-shaped clouds contain the raw material for star formation and hide protostars within. Nearly 3,000 light-years distant, the relatively faint IC 1396 complex covers a large region on the sky, spanning over 5 degrees. This rendition spans a 1 degree wide field of view though, about the angular size of 2 full moons. Of course the dark shapes below and to the right of the outstretched Elephant's Trunk, are known to some as The Caravan.
-        </p>
-    </section>`;
-  }
 
+ 
